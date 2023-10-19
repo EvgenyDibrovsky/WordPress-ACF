c6738984cf30655c3964cac39298ae188499e2dd
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-10-19 21:48:24 +0200

    commit

--- ACF.php
+++ ACF.php
@@ -1,3 +1,29 @@
+<!-- Text -->
+<?php
+$hp_section_1_title = get_field('hp-section-1-title');
+if ($hp_section_1_title) : ?>
+    <h2 class="text-titleh2Mobile lg:text-titleh2 font-bold text-textBlack opacity-0 init-animate-5-5" data-delay="0.1s" data-time="1s"><?php echo $hp_section_1_title; ?></h2>
+<?php endif; ?>
+
+<!-- Btn -->
+<?php
+$link = get_field('hp-section-3-btn');
+if ($link) :
+    $link_url = esc_url($link['url']);
+    $link_title = esc_html($link['title']);
+    $link_target = $link['target'] ? esc_attr($link['target']) : '_self';
+?>
+    <a class="btn-yellow opacity-0 init-animate-2" href="<?php echo $link_url; ?>" target="<?php echo $link_target; ?>"><?php echo $link_title; ?></a>
+<?php endif; ?>
+
+<!-- img -->
+<?php
+$image = get_field('investment-section-3-image');
+if (!empty($image)) :
+    $url = $image['url'];
+    echo '<img src="' . $url . '" alt="' . $image['alt'] . '" class="w-full h-auto  " />';
+endif;
+?>
 <!-- swiper -->
 <?php if (have_rows('hero-slider-homepage')) : ?>
     <div class="swiper mySwiper">
